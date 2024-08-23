45f10ddc194629727f8ba70b52f1faaf148c18d9
panthil malaviya <panthil@dstrctgroup.com>
2024-08-23 12:14:33 +0530

    try to live

--- src/views/auth/SignIn/index.js
+++ src/views/auth/SignIn/index.js
@@ -3,16 +3,15 @@ import React from "react";
 import { useSelector } from "react-redux";
 import SignInForm from "./signInForm.js";
 const SignIn = () => {
-  const themeColor = useSelector((state) => state?.theme?.themeColor);
-  const primaryColorLevel = useSelector(
-    (state) => state?.theme?.primaryColorLevel
-  );
+ 
 
   return (
     <>
       <div className="mt-2 mb-8">
         <p
-          className={`mb-1 text-xl font-bold text-${themeColor}-${primaryColorLevel}`}
+          // className={`mb-1 text-xl font-bold text-${themeColor}-${primaryColorLevel}`}
+          className={`mb-1 text-xl font-bold `}
+
         >
           Welcome To LMS
         </p>
