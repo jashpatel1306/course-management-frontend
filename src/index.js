c1bf88170d4168748c77d187446b5d2783745cce
Krunal-L3 <krunal.laughlogiclabs@gmail.com>
2025-01-13 10:05:36 +0530

    script fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import "./app.min.css";
-import './index.css';
+import "./app.min.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
