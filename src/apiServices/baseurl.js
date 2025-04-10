7aa3d31d50dcec3f021955a08b21dc7cb0662f37
jashpatel1306 <jashpatel1306@gmail.com>
2025-04-10 12:14:41 +0530

    change ENV

--- src/apiServices/baseurl.js
+++ src/apiServices/baseurl.js
@@ -2,14 +2,14 @@
 // export const BACKEND_BASE_URL = process.env.REACT_APP_URL;
 // https://course-management-backend-zu3m.onrender.com
 
-export const BACKEND_BASE_URL = "http://localhost:3030/";
-export const FRONTEND_BASE_URL = "http://localhost:3000";
+// export const BACKEND_BASE_URL = "http://localhost:3030/";
+// export const FRONTEND_BASE_URL = "http://localhost:3000";
 
 // export const BACKEND_BASE_URL ="https://course-management-backend-zu3m.onrender.com";
 // export const FRONTEND_BASE_URL = "http://43.205.25.25";
 
-// export const BACKEND_BASE_URL = "https://lms.velths.com";
-// export const FRONTEND_BASE_URL = "http://43.205.25.25";
+export const BACKEND_BASE_URL = "https://lms.velths.com";
+export const FRONTEND_BASE_URL = "http://43.205.25.25";
 
 // export const BACKEND_BASE_URL ="https://lms.velths.com/";
 // export const FRONTEND_BASE_URL = "http://43.205.25.25";
